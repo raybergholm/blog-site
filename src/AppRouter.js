38e8f780c306b1f7084e8870a7cec1616559a86b
Ray Bergholm <nalamint@gmail.com>
2018-05-13 23:02:06 +0300

    bugfix invalid refs from refactor

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,14 +5,14 @@ import AboutView from "./pages/AboutView";
 
 import FeedView from "./pages/FeedView";
 import ArchiveView from "./pages/ArchiveView";
-import PostView from "./pages/ArticleView";
+import BlogPostView from "./pages/BlogPostView";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={FeedView} />
       <Route exact path="/about" component={AboutView} />
-      <Route path="/post/:postId" component={PostView} />
+      <Route path="/post/:postId" component={BlogPostView} />
       <Route path="/archive/:year/:month" component={ArchiveView} />
       <Redirect path="/blog/*" to="/" />
       <Redirect path="*" to="/" />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
-import rootReducer from "./reducers/RootReducer";
+import rootReducer from "./reducers/rootReducer";
 
 import "./index.css";
 
