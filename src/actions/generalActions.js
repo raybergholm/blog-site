f65b4cc48342b60f7017e11221346df37ec501e2
Ray Bergholm <raybergholm@gmail.com>
2018-05-16 13:17:27 +0300

    bugfix function name

--- src/actions/generalActions.js
+++ src/actions/generalActions.js
@@ -2,6 +2,6 @@ export const initialize = () => ({
   type: "INITIALIZE_START"
 });
 
-export const initialisationComplete = () => ({
+export const initializationComplete = () => ({
   type: "INITIALIZE_END"
 });
