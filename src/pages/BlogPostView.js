dbec4603321c503146858b99f0f179064790ae2b
Ray Bergholm <raybergholm@gmail.com>
2018-05-22 23:16:00 +0300

    refactor fullPost state

--- src/pages/BlogPostView.js
+++ src/pages/BlogPostView.js
@@ -4,26 +4,19 @@ import PropTypes from "prop-types";
 import BlogPostNavigation from "../components/BlogPostNavigation";
 import BlogPost from "../components/BlogPost";
 
-const fetchPost = (articleId) => {
-  // const result = mockPosts.filter(({ _id }) => _id === articleId);
-
-  // return result ? result[0] : null;
-};
-
-const Page = ({ match }) => {
-  const post = fetchPost(match.params.articleId);
-
-  return (
-    <div id="main-content-section">
-      <BlogPostNavigation prevLink={post.prev} nextLink={post.next} />
-      {post && (<BlogPost key={post._id} _id={post._id} {...post.content} />)}
-    </div>
-  );
-};
+const Page = ({ fullPost }) => (
+  <div id="main-content-section">
+    {fullPost && (
+      <div>
+        <BlogPostNavigation prevLink={fullPost.prev} nextLink={fullPost.next} />
+        <BlogPost key={fullPost._id} _id={fullPost._id} {...fullPost.content} />
+      </div>
+    )}
+  </div>
+);
 
 Page.propTypes = {
-  match: PropTypes.object,
-  state: PropTypes.object
+  fullPost: PropTypes.object
 };
 
 export default Page;
