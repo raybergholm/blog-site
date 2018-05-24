6b708cc4a7a538d18748cfd3c6d7a7d62474a655
Ray Bergholm <nalamint@gmail.com>
2018-05-24 23:03:22 +0300

    refactor BlogPostView to handle null input

--- src/pages/BlogPostView.js
+++ src/pages/BlogPostView.js
@@ -4,19 +4,27 @@ import PropTypes from "prop-types";
 import BlogPostNavigation from "../components/BlogPostNavigation";
 import BlogPost from "../components/BlogPost";
 
-const Page = ({ fullPost }) => (
-  <div id="main-content-section">
+const fetchPost = (postId) => {
+
+};
+
+const Page = ({ match }) => {
+  const postId = match && match.params ? match.params.currentPost : null;
+
+  const fullPost = fetchPost(postId);
+
+  return (<div id="main-content-section">
     {fullPost && (
       <div>
         <BlogPostNavigation prevLink={fullPost.prev} nextLink={fullPost.next} />
         <BlogPost key={fullPost._id} _id={fullPost._id} {...fullPost.content} />
       </div>
     )}
-  </div>
-);
+  </div>);
+};
 
 Page.propTypes = {
-  fullPost: PropTypes.object
+  match: PropTypes.object
 };
 
 export default Page;
