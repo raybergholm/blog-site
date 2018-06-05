8a0762addbf7193b27e93800e48a4dbf18c3a095
Ray Bergholm <raybergholm@gmail.com>
2018-06-05 14:32:29 +0300

    fix BlogPost model

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -2,7 +2,7 @@ const DEFAULTS = {
   Title: "PLACEHOLDER TITLE",
   Author: "PLACEHOLDER AUTHOR",
   Summary: "PLACEHOLDER SUMMARY",
-  Text: "PLACEHOLDER TEXT",
+  Body: "PLACEHOLDER TEXT",
   YearMonthKey: "2000-01",
   PublishDate: "2000-01-01:00:00:00Z",
   RestUrlKey: "Placeholder",
@@ -11,12 +11,12 @@ const DEFAULTS = {
 
 class BlogPost {
   constructor(params) {
-    const { title, author, summary, text, yearMonthKey, publishDate, restUrlKey, tags } = params;
+    const { title, author, summary, body, yearMonthKey, publishDate, restUrlKey, tags } = params;
 
     this.title = title || DEFAULTS.Title;
     this.author = author || DEFAULTS.Author;
     this.summary = summary || DEFAULTS.Summary;
-    this.text = text || DEFAULTS.Text;
+    this.body = body || DEFAULTS.Body;
     this.yearMonthKey = yearMonthKey || DEFAULTS.YearMonthKey;
     this.publishDate = publishDate || DEFAULTS.PublishDate;
     this.restUrl = restUrlKey || DEFAULTS.RestUrlKey;
